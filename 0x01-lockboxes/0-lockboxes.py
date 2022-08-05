d423320fb1b2e035bab4b65354da43e97473679c
oangel26 <oa.angel26@gmail.com>
2022-08-04 20:44:40 -0500

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
+"""
+
+
 def canUnlockAll(boxes):
     """ method that determines if all the boxes can be opened."""
     if boxes is None or len(boxes) == 0:
@@ -25,5 +32,11 @@ def canUnlockAll(boxes):
 
 
 if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
     print(canUnlockAll(boxes))
