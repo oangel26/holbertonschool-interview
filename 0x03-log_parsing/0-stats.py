91df7b5adaee97670188c1a76d64dfacb2c317ca
oangel26 <oa.angel26@gmail.com>
2022-08-18 09:02:46 -0500

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,6 +9,13 @@ import json
 import time
 
 
+def print_stats(file_size: int, status_dict: dict) -> None:
+    """ Function which prints stats """
+    print(f"File size: {file_size}")
+    for s_code, s_number in sorted(status_dict.items()):
+        print(f"{s_code}: {str(s_number)}")
+
+
 counter = 0
 status_dict = {}
 file_size = 0
@@ -18,16 +25,16 @@ try:
         chunk = line.split()
         status_code = json.loads(chunk[7])
         file_size += int(chunk[8])
-        if (isinstance(status_code, int) and status_code in status_dict):
+
+        # Creates status dictionary
+        if (status_code in status_dict):
             status_dict[status_code] += 1
         else:
             status_dict[status_code] = 1
+
         if counter % 10 == 0:
-            print(f"File size: {file_size}")
-            for k, v in sorted(status_dict.items()):
-                print(f"{k}: {v}")
+            print_stats(file_size, status_dict)
+
 except KeyboardInterrupt:
-    print(f"File size: {file_size}")
-    for k, v in sorted(status_dict.items()):
-        print(f"{k}: {v}")
-    sys.exit(0)
+    print_stats(file_size, status_dict)
+    raise
