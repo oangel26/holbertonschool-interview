70b0f3d2c06f14a5910e3b487e4aa811648b9ccc
oangel26 <oa.angel26@gmail.com>
2022-08-17 23:09:12 -0500

    first commit

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Log parsing
+"""
+
+
+import sys
+
+
+counter = 0
+status_dict = {}
+file_size = 0
+try:
+    for line in sys.stdin:
+        counter += 1
+        chunk = line.split()
+        file_size += int(chunk[8])
+        if chunk[7] in status_dict:
+            status_dict[chunk[7]] += 1
+        else:
+            status_dict[chunk[7]] = 1
+        if counter % 10 == 0:
+            print(f"File size: {file_size}")
+            for k, v in status_dict.items():
+                print(f"{k}: {v}")
+except KeyboardInterrupt:
+    print(f"File size: {file_size}")
+    for k, v in status_dict.items():
+        print(f"{k}: {v}")
+        sys.exit(0)
