f4f342a3ff363c5cb0689fc14db611efc7527735
oangel26 <oa.angel26@gmail.com>
2022-08-18 07:45:16 -0500

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,6 +5,8 @@ Log parsing
 
 
 import sys
+import json
+import time
 
 
 counter = 0
@@ -14,11 +16,12 @@ try:
     for line in sys.stdin:
         counter += 1
         chunk = line.split()
+        status_code = json.loads(chunk[7])
         file_size += int(chunk[8])
-        if chunk[7] in status_dict:
-            status_dict[chunk[7]] += 1
+        if (isinstance(status_code, int) and status_code in status_dict):
+            status_dict[status_code] += 1
         else:
-            status_dict[chunk[7]] = 1
+            status_dict[status_code] = 1
         if counter % 10 == 0:
             print(f"File size: {file_size}")
             for k, v in sorted(status_dict.items()):
@@ -27,4 +30,4 @@ except KeyboardInterrupt:
     print(f"File size: {file_size}")
     for k, v in sorted(status_dict.items()):
         print(f"{k}: {v}")
-        sys.exit(0)
+    sys.exit(0)
