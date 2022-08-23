175ddf6701e99e17f98b0fecea9b0d3b40031c3e
oangel26 <oa.angel26@gmail.com>
2022-08-22 21:20:07 -0500

    first commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """ Determines if a given data set represents a valid UTF-8 encoding
+    """
+    for i in data:
+        if i > 255 or i < 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))
