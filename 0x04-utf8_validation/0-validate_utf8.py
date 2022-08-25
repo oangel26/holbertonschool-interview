873badee3b01a32d917ca19c04750ab83095b034
oangel26 <oa.angel26@gmail.com>
2022-08-25 08:08:13 -0500

    decode in utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,16 +8,18 @@ def validUTF8(data):
     """ Determines if a given data set represents a valid UTF-8 encoding
     """
     try:
-        bytes(data)
-    except:
+        bytes(data).decode('utf8')
+    except Exception:
         return False
     return True
+
 
 if __name__ == "__main__":
     data = [65]
     print(validUTF8(data))
 
-    data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+    data = [80, 121, 116, 104, 111, 110, 32, 105,
+            115, 32, 99, 111, 111, 108, 33]
     print(validUTF8(data))
 
     data = [229, 65, 127, 256]
