c47f6889ce1cfa53f7049eca70396e99db63df85
oangel26 <oa.angel26@gmail.com>
2022-10-18 22:16:24 +0100

    This is not my code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,36 +9,28 @@ def minOperations(n):
     a number n, write a method that calculates the fewest number of
     operations needed to result in exactly n H characters in the file.
     """
-    s_file = 'H'
-    num_operation = 1
-    def copy_all(new_file):
-        s_file = new_file
-        num_operation += 1
-        return s_file
+    def minOperations(n):
+    """
+    This is a method that calculates the fewest number of
+    operations needed to result in exactly n H characters
+    in the file.
+    """
+    result = 0
+    i = 2
 
-    def paste(s_file):
-        tmp = s_file
-        s_file = tmp * 2
-        num_operation += 1
-        return s_file
-
-    if (n == 0):
+    if isinstance(n, int) and n < 2:
         return 0
-    elif (n == 1):
-        return num_operation
-    elif (n % 3 == 0):
-        for i in range(int(n/3) - 1):
-            paste(s_file)
-            copy_all(s_file)
-            paste(s_file)
-            paste(s_file)
-    elif (n % 2 == 0):
-        for i in range(int(n/2) - 1):
-            paste(s_file)
-            copy_all(s_file)
-            paste(s_file)
 
-    return num_operation
+    while i <= n + 1:
+        if n % i == 0:
+            result += i
+            n //= i
+            i = 2
+        else:
+            i += 1
+
+    return result
+
 
 if __name__ == "__main__":
     """
