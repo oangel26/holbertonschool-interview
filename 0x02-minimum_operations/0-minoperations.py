3bfe54fee7eb6673be80a1edbbbc91e9f7f205cc
oangel26 <oa.angel26@gmail.com>
2022-10-18 22:19:43 +0100

    This is not my code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,42 +3,39 @@
 
 
 def minOperations(n):
-    """
-    In a text file, there is a single character H. Your text editor can
-    execute only two operations in this file: Copy All and Paste. Given
-    a number n, write a method that calculates the fewest number of
-    operations needed to result in exactly n H characters in the file.
-    """
-    def minOperations(n):
-    """
-    This is a method that calculates the fewest number of
-    operations needed to result in exactly n H characters
-    in the file.
-    """
-    result = 0
-    i = 2
+     """
+     In a text file, there is a single character H. Your text editor can
+     execute only two operations in this file: Copy All and Paste. Given
+     a number n, write a method that calculates the fewest number of
+     operations needed to result in exactly n H characters in the file.
+     """
+     s_file = 'H'
+     num_operation = 1
+     def copy_all(new_file):
+         s_file = new_file
+         num_operation += 1
+         return s_file
 
-    if isinstance(n, int) and n < 2:
-        return 0
+     def paste(s_file):
+         tmp = s_file
+         s_file = tmp * 2
+         num_operation += 1
+         return s_file
 
-    while i <= n + 1:
-        if n % i == 0:
-            result += i
-            n //= i
-            i = 2
-        else:
-            i += 1
+     if (n == 0):
+         return 0
+     elif (n == 1):
+         return num_operation
+     elif (n % 3 == 0):
+         for i in range(int(n/3) - 1):
+             paste(s_file)
+             copy_all(s_file)
+             paste(s_file)
+             paste(s_file)
+     elif (n % 2 == 0):
+         for i in range(int(n/2) - 1):
+             paste(s_file)
+             copy_all(s_file)
+             paste(s_file)
 
-    return result
-
-
-if __name__ == "__main__":
-    """
-    Main code for testing
-    """
-
-    n = 4
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+     return num_operation
