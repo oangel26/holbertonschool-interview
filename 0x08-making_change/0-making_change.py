604185691f91925f928a1d4fdd17449715caa79f
oangel26 <oa.angel26@gmail.com>
2022-10-21 21:17:46 +0100

    Not my code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,31 +1,48 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values, determine the fewest number of
-coins needed to meet a given amount total
+Given a pile of coins of different values, determine
+the fewest number of coins needed to meet a given
+amount total.
 """
 
 
-def makeChange(coins: list, total: int) -> int:
+def makeChange(coins, total, acumulated=0, ordered=False):
     """
-    Function that  determine the fewest number of coins needed
-    to meet a given amount total.
-        Returns:
-            Fewest number of coins needed to meet total.
-            If total is 0 or less, return 0.
-            If total cannot be met by any number of coins you have,
-            return -1.
+    Given a pile of coins of different values, determine
+    the fewest number of coins needed to meet a given
+    amount total.
     """
-    if total <= 0:
+    if len(coins) == 0 and total > 0:
+        return -(acumulated + 1)
+
+    if len(coins) == 0:
         return 0
 
-    new_coins = []
-    coins.sort(reverse=True)
-    div = 0
+    if not ordered:
+        coins.sort(reverse=True)
 
-    for coin in coins:
-        div = total // coin
-        total = total % coin
-        new_coins.append(div)
-    if total != 0:
+    biggest, *leftover = coins
+
+    if type(total) is not int or type(biggest) is not int:
         return -1
-    return sum(new_coins)
+
+    if total == 0:
+        return 0
+
+    possibleAmount = (total // biggest)
+
+    if possibleAmount == 0:
+        return makeChange(leftover, total, acumulated)
+
+    nextTotal = (total % biggest)
+    nextChanges = possibleAmount + \
+        makeChange(leftover, nextTotal, acumulated + possibleAmount, ordered)
+    if nextChanges == -1:
+        nextChanges = makeChange(leftover, total, acumulated, ordered)
+    return nextChanges
+
+
+if __name__ == '__main__':
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    print(makeChange([2, 21], 26))
